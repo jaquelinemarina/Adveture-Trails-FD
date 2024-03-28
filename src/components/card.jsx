f681114db3f7140cb84e5a8426b72b673c6456e4
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-03-28 18:51:40 -0300

    Ex 4 - Adição de props no CardTrilha

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,14 @@
-function Card() {
+function Card(props) {
     retur(
         <>
+            <p>{props.nameTrail}</p>
+            <p>{props.region}</p>
+            <p>{props.duration}</p>
+            <p>{props.path}</p>
+            <p>{props.difficulty}</p>
+            <p>{props.typeTrail}</p>
+            <p>{props.owner}</p>
+            <p>{props.URLimg}</p>
         </>
     )
 }
