ee5e6be55edb392b7cbee29a509b73e1c03c2b11
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-03 12:08:49 -0300

    update Semana 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,67 @@
-import Card from "./components/card.jsx"
+import CardTrilha from "./components/card.jsx"
+import "./App.css"
 
 function App() {
+
+  const listaTrilhas = [
+    {
+      nomeTrilha: "Trilha da Costa da Lagoa",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 90,
+      trajeto: 4,
+      dificuldade: "Iniciante",
+      tipo: "Hiking",
+      nomeUsuario: "Jaqueline",
+      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+    ,
+    {
+      nomeTrilha: "Trilha do Gravatá",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 40,
+      trajeto: 1.4,
+      dificuldade: "Fácil",
+      tipo: "Hiking",
+      nomeUsuario: "Jaqueline",
+      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+    ,
+    {
+      nomeTrilha: "Trilha de Naufragados",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 120,
+      trajeto: 7,
+      dificuldade: "Moderada",
+      tipo: "Treking",
+      nomeUsuario: "Jaqueline",
+      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+    ,
+    {
+      nomeTrilha: "Trilha do Vale da Utopia",
+      cidade: "Pinheira",
+      estado: "SC",
+      duracao: 210,
+      trajeto: 5.5,
+      dificuldade: "Moderada",
+      tipo: "Treking",
+      nomeUsuario: "Jaqueline",
+      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+  ]
+
   return (
-    <>
-      <Card nameTrail="" region="" duration="" path="" difficulty="" tipeTrail="" owner="" URLimg="" />
-    </>
+    <div className="container">
+      <h1 className="titulo">Explore trilhas incríveis</h1>
+      {
+        listaTrilhas.map((trilha, index) => (
+          <CardTrilha dadosTrilha={trilha} key={index} />
+        ))
+      }
+    </div>
   )
 }
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,27 +1,42 @@
-import * as PropTypes from "prop-types"
-import "./style.css"
+import * as PropTypes from "prop-types";
+import "./card.css";
 
-function CardTrail({ trailInfo }) {
-    return (
-        <>
-            <h1>{trailInfo.trailName}</h1>
-            <span>{trailInfo.city}, {trailInfo.state}</span>
-        </>
-    )
+function CardTrilha({ dadosTrilha }) {
+  return (
+    <div className="card_container">
+      <img
+        className="card_imagem"
+        width={200}
+        src={dadosTrilha.urlImagem}
+        alt="imagem trilha"
+      />
+        <div className="text_container">
+      <h1>{dadosTrilha.nomeTrilha}</h1>
+        <p>Trajeto: {dadosTrilha.trajeto} km</p>
+        <p>Duração: {dadosTrilha.duracao} minutos</p>
+        <p>Dificuldade: {dadosTrilha.dificuldade}</p>
+        <p>Tipo: {dadosTrilha.tipo}</p>
+      </div>
+      <span>
+        {dadosTrilha.cidade} / {dadosTrilha.estado}
+      </span>
+    </div>
+  );
 }
 
-CardTrail.propTypes = {
-    trailInfo: PropTypes.exact({
-        trailName: PropTypes.string.isRequired,
-        city: PropTypes.string.isRequired,
-        state: PropTypes.string.isRequired,
-        duration: PropTypes.number.isRequired,
-        path: PropTypes.number.isRequired,
-        difficulty: PropTypes.string.isRequired,
-        trailType: PropTypes.oneOf(['Hiking', 'Trekking', 'Ciclismo']),
-        owner: PropTypes.string.isRequired,
-        urlImg: PropTypes.string.isRequired,
-    })
-}
+// configuração das props types
+CardTrilha.propTypes = {
+  dadosTrilha: PropTypes.exact({
+    nomeTrilha: PropTypes.string.isRequired,
+    cidade: PropTypes.string.isRequired,
+    estado: PropTypes.string.isRequired,
+    duracao: PropTypes.number.isRequired,
+    trajeto: PropTypes.number.isRequired,
+    dificuldade: PropTypes.string.isRequired,
+    tipo: PropTypes.oneOf(["Hiking", "Trekking", "Ciclismo"]),
+    nomeUsuario: PropTypes.string.isRequired,
+    urlImagem: PropTypes.string.isRequired,
+  }),
+};
 
-export default CardTrail
+export default CardTrilha;
