0d1c80f805e6a3aaa272e5d2bc7e817b8c570e85
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-04-13 12:45:00 -0300

    style: header e footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 function Header() {
     return (
         <header className={style.container}>
-            <Link to="/" className={style.home} underline="none">Adventure Trails FD </Link>
-            <Link to="/Lista" className={style.list}  underline="none">Explorar Trilhas</Link>
-            <Link to="/Cadastro" className={style.config} underline="none">Cadastrar Trilhas</Link>
+            <Link to="/" className={style.home}>Adventure Trails FD </Link>
+            <Link to="/Lista" className={style.list}>Explorar Trilhas</Link>
+            <Link to="/Cadastro" className={style.register}>Cadastrar Trilhas</Link>
         </header>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,8 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
-
-//importação das rotas
 import routers from './router/routes'
-
-
-// themes do material ui
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            light: '#757ce8',
-            main: '#3f50b5',
-            dark: '#079f20',
-            contrastText: '#fff',
-        },
-        secondary: {
-            light: '#ff7961',
-            main: '#f44336',
-            dark: '#ba000d',
-            contrastText: '#000',
-        },
-    },
-});
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
